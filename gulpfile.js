6f7061bab45bf6ecc7439bfa8c2a6cb954815307
Geoff Agnew <gagnew@gmail.com>
2017-12-27 16:00:07 -0500

    Added browserify and gulp-sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     gconcat = require('gulp-concat'),
+    browserify = require('browserify'),
+    sourcemaps = require('gulp-sourcemaps'),
     coffee = require('gulp-coffee');
 
 var jsSources = [
@@ -10,11 +12,29 @@ var jsSources = [
   'components/scripts/template.js',
 ];
 
-gulp.task('coffee', function() {
-  gulp.src('components/coffee/tagline.coffee')
-    .pipe(coffee({ bare: true })
-      .on('error', gutil.log))
-    .pipe(gulp.dest('components/scripts'))
+// gulp.task('coffee', function() {
+//   gulp.src('components/coffee/tagline.coffee')
+//     .pipe(coffee({ bare: true })
+//       .on('error', gutil.log))
+//     .pipe(gulp.dest('components/scripts'))
+// });
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './entry.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('concat', function() {
