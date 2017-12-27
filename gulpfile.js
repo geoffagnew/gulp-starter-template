5ebd74dd36b0fec6a661b7e11ccf33bd4fda8129
Geoff Agnew <gagnew@gmail.com>
2017-12-27 15:00:25 -0500

    Added new gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    gconcat = require('gulp-concat'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Is this working?');
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js',
+];
+
+gulp.task('coffee', function() {
+  gulp.src('components/coffee/tagline.coffee')
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('concat', function() {
+  return gulp.src(jsSources)
+    .pipe(gconcat('script.js'))
+    .pipe(gulp.dest('builds/development/js'));
 });
